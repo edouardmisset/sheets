7f3dcc4cdbfdb99c014a5356737cc67f85cc973b
Edouard Misset <edouardmisset@gmail.com>
2021-07-04 21:23:17 +0200

    routes route working

--- app.js
+++ app.js
@@ -1,38 +1,47 @@
-const express = require('express')
-const { google } = require('googleapis')
+const express = require('express');
+const { google } = require('googleapis');
 
-require('dotenv').config()
+require('dotenv').config();
 
-const app = express()
+const app = express();
 
-const PORT = process.env.PORT
+const PORT = process.env.PORT;
 
-const port = PORT || 5000
+const port = PORT || 5000;
 
-app.use(express.json())
-app.set('x-powered-by', false) // for security
-app.set('trust proxy', 1) // trust first proxy
+app.use(express.json());
+app.set('x-powered-by', false); // for security
+app.set('trust proxy', 1); // trust first proxy
 
 const server = app.listen(port, () => {
-  console.log(`Server is running on port: ${port}`)
-})
+  console.log(`Server is running on port: ${port}`);
+});
 
 // process setup : improves error reporting
-process.on('unhandledRejection', (error) => {
-  console.error('unhandledRejection', JSON.stringify(error), error.stack)
-})
-process.on('uncaughtException', (error) => {
-  console.error('uncaughtException', JSON.stringify(error), error.stack)
-})
+process.on('unhandledRejection', error => {
+  console.error('unhandledRejection', JSON.stringify(error), error.stack);
+});
+process.on('uncaughtException', error => {
+  console.error('uncaughtException', JSON.stringify(error), error.stack);
+});
 
 app.get('/routes', async (req, res) => {
   try {
-    res.send('hi')
+    const auth = await google.auth.getClient({
+      scopes: ['https://www.googleapis.com/auth/spreadsheets.readonly'],
+    });
+    const sheets = google.sheets({ version: 'v4', auth });
+    const range = 'ascents!A1:O385';
+    const response = await sheets.spreadsheets.values.get({
+      spreadsheetId: process.env.SHEET_ID,
+      range,
+    });
+    const [header, ...ascents] = response.data.values;
+    res.send({ header, ascents });
   } catch (err) {
-    console.error(err)
-  } finally {
-
+    console.error(err);
+    res.status(500).send(err);
   }
-})
+});
 
-module.exports = server
+module.exports = server;
